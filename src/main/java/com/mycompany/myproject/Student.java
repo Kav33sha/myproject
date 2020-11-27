b6998bc758926ba837676036caeff1e4f1add211
Kav33sha <kaveesha.lakdinu15061@mail.com>
2020-11-27 16:41:10 +0530

    myproject

---
+++ src/main/java/com/mycompany/myproject/Student.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myproject;
+
+/**
+ *
+ * @author kaveesha
+ */
+public class Student {
+    public static final String STUDENT_CODE = "ST";
+    
+    public String firstName = "Kaveesha";
+    public String lastName = "Lakdinu";
+    
+}
---
+++ src/main/java/newpackage/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author kaveesha
+ */
+public class App {
+     public static void main(String [] args){
+         
+         Student st = new Student();
+         st.setFirstName("Kaveesha");
+         System.out.println(st.getFirstName());
+     }
+}
